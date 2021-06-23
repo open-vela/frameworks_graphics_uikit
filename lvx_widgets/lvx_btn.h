a5cef8aaef05c16532d53bc0e631b692a9561cc6
wangxuedong <wangxuedong@xiaomi.com>
2021-06-23 20:13:27 +0800

    Add lvx_btn and lvx_radio widgets.
    N/A
    
    Signed-off-by: wangxuedong <wangxuedong@xiaomi.com>

---
+++ lvx_widgets/lvx_btn.h
@@ -0,0 +1,53 @@
+#ifndef LV_EXTENSIONS_WIDGETS_BTN_H_
+#define LV_EXTENSIONS_WIDGETS_BTN_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*********************
+ *      INCLUDES
+ *********************/
+#include "../lv_ext_conf.h"
+
+#include "lvx_widgets.h"
+
+#if (LVX_USE_BTN != 0 && LV_USE_BTN != 0)
+
+/*********************
+ *      DEFINES
+ *********************/
+
+/**********************
+ *      TYPEDEFS
+ **********************/
+
+
+typedef struct {
+    lv_btn_t btn;
+}lvx_btn_t;
+
+extern const lv_obj_class_t lvx_btn_class;
+
+/**********************
+ * GLOBAL PROTOTYPES
+ **********************/
+
+/* lvx_btn api */
+lv_obj_t* lvx_btn_create(lv_obj_t* parent);
+void lvx_btn_set_img_src(lv_obj_t* obj, void* src);
+void lvx_btn_set_text_fmt(lv_obj_t* obj, char* fmt, ...);
+void lvx_btn_set_style_bg_color(lv_obj_t* obj, lv_color_t color);
+void lvx_btn_set_style_text_color(lv_obj_t* obj, lv_color_t color);
+
+/**********************
+ *      MACROS
+ **********************/
+
+#endif /* LVX_USE_BTN && LV_USE_BTN */
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif /* LV_EXTENSIONS_WIDGETS_BTN_H_ */
---
+++ lvx_widgets/lvx_radio.h
@@ -0,0 +1,52 @@
+#ifndef LV_EXTENSIONS_WIDGETS_RADIO_H_
+#define LV_EXTENSIONS_WIDGETS_RADIO_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*********************
+ *      INCLUDES
+ *********************/
+#include "lvx_widgets.h"
+#include "lvx_btn.h"
+
+#if (LVX_USE_RADIO != 0)
+
+/*********************
+ *      DEFINES
+ *********************/
+
+/**********************
+ *      TYPEDEFS
+ **********************/
+
+
+typedef struct {
+    lvx_btn_t btn;
+    uint32_t checked : 1;
+    uint32_t reserved : 31;
+}lvx_radio_t;
+
+extern const lv_obj_class_t lvx_radio_class;
+
+/**********************
+ * GLOBAL PROTOTYPES
+ **********************/
+
+/* lvx_radio api */
+lv_obj_t* lvx_radio_create(lv_obj_t* parent);
+void lvx_radio_set_img_src(lv_obj_t* obj, void* src);
+void lvx_radio_set_style_bg_color(lv_obj_t* obj, lv_color_t color);
+
+/**********************
+ *      MACROS
+ **********************/
+
+#endif /* LVX_USE_RADIO */
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif /* LV_EXTENSIONS_WIDGETS_RADIO_H_ */
