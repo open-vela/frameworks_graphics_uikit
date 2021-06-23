a649ef62560013ecdf2cc8a802393bfaab9b5ee4
wangxuedong <wangxuedong@xiaomi.com>
2021-06-23 20:13:27 +0800

    Add lvx_btn and lvx_radio widgets.
    N/A
    
    Signed-off-by: wangxuedong <wangxuedong@xiaomi.com>

--- lv_ext.h
+++ lv_ext.h
@@ -3,6 +3,9 @@
 
 #include "lv_ext_conf.h"
 #include "lvx_themes/lvx_theme_extended.h"
+#include "lvx_widgets/lvx_btn.h"
+#include "lvx_widgets/lvx_radio.h"
+
 
 #else
 #error "The external file has defined LV_EXT_H"
