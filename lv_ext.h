5275aa550ccf0ff174acc6025eb46dc183f9de43
qinshijing <qinshijing@xiaomi.com>
2021-06-23 19:57:48 +0800

    Initialize, create template
    N/A
    
    1. Added Makefile/Make.defs/Kconfig
    2. Added theme and lvx_widgets
    
    Signed-off-by: qinshijing <qinshijing@xiaomi.com>

---
+++ lv_ext.h
@@ -0,0 +1,9 @@
+#ifndef LV_EXT_H
+#define LV_EXT_H
+
+#include "lv_ext_conf.h"
+#include "lvx_themes/lvx_theme_extended.h"
+
+#else
+#error "The external file has defined LV_EXT_H"
+#endif /* LV_EXT_H */
