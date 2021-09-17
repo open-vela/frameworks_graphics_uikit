4ee5f67f2125dc177722f3cbaf608db39a836bec
HaoHangyu <haohangyu@xiaomi.com>
2021-09-17 10:44:32 +0800

    Fix: lvx_radio need group.
    Change-Id: I01400a70095ea2001d8c79fbe8766359caa896db

--- lvx_widgets/lvx_radio.h
+++ lvx_widgets/lvx_radio.h
@@ -20,11 +20,14 @@ extern "C" {
 /**********************
  *      TYPEDEFS
  **********************/
-
+typedef struct {
+    lv_ll_t obj_list;
+} lvx_radio_group_t;
 
 typedef struct {
     lvx_btn_t btn;
-}lvx_radio_t;
+    lvx_radio_group_t* group;
+} lvx_radio_t;
 
 extern const lv_obj_class_t lvx_radio_class;
 
@@ -33,6 +36,12 @@ extern const lv_obj_class_t lvx_radio_class;
  **********************/
 
 /* lvx_radio api */
+lvx_radio_group_t* lvx_radio_group_create(void);
+void lvx_radio_group_destroy(lvx_radio_group_t* group);
+void lvx_radio_group_add_radio(lvx_radio_group_t* group, lv_obj_t* obj);
+void lvx_radio_group_remove_radio(lvx_radio_group_t* group, lv_obj_t* obj);
+void lvx_radio_set_state(lv_obj_t* obj, lv_state_t state);
+
 lv_obj_t* lvx_radio_create(lv_obj_t* parent);
 void lvx_radio_set_img_src(lv_obj_t* obj, const void* src);
 void lvx_radio_set_style_bg_color(lv_obj_t* obj, lv_color_t color);
