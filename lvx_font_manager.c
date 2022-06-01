06a38ee9a5b3885636671195d0ed67e4f017a87a
pengyiqiang <pengyiqiang@xiaomi.com>
2022-06-01 20:02:08 +0800

    lvgl/ext: font manager adds emoji fallback
    Change-Id: I6d64c2aa3505e4af11be58e22e072fe59001124c

--- lvx_font_manager.c
+++ lvx_font_manager.c
@@ -54,18 +54,27 @@ static void ft_in_fts_del(const lv_font_t * delfont);
 static void fts_list_clear(void);
 static bool font_name_combine(char * buff, uint16_t buff_len, const char * name);
 
+#if defined(CONFIG_FONT_USE_EMOJI)
+static bool emoji_get_imgfont_cb(const lv_font_t* font, void* img_src, uint16_t len,
+                                 uint32_t unicode, uint32_t unicode_next);
+#endif
+
 /**********************
  *  STATIC VARIABLES
  **********************/
 
 /* save global fonts info */
-static fonts_info_t font_tables;
+static fonts_info_t g_font_tables;
 
 /* font path */
-static char * font_base_path = NULL;
+static char * g_font_base_path = NULL;
 
 /* first initialization flag */
-static bool init_flag = false;
+static bool g_init_flag = false;
+
+#if defined(CONFIG_FONT_USE_EMOJI)
+static lv_font_t * g_emoji_font = NULL;
+#endif
 
 /**********************
  *   GLOBAL FUNCTIONS
@@ -73,14 +82,14 @@ static bool init_flag = false;
 
 void lvx_font_init(void)
 {
-    if(init_flag) {
+    if(g_init_flag) {
         return;
     }
 
-    _lv_ll_init(&font_tables.ft_ll, sizeof(font_refer_t));
-    _lv_ll_init(&font_tables.name_ll, sizeof(name_refer_t));
+    _lv_ll_init(&g_font_tables.ft_ll, sizeof(font_refer_t));
+    _lv_ll_init(&g_font_tables.name_ll, sizeof(name_refer_t));
 
-    if(!font_base_path) {
+    if(!g_font_base_path) {
         const uint16_t buff_len = 256;
         char * buff = lv_mem_alloc(buff_len);
         LV_ASSERT_MALLOC(buff);
@@ -98,32 +107,54 @@ void lvx_font_init(void)
         lv_mem_free(buff);
     }
 
-    init_flag = true;
+#if defined(CONFIG_FONT_USE_EMOJI)
+    lv_font_t * imgfont = lv_imgfont_create(CONFIG_FONT_EMOJI_SIZE_DEFAULT,
+                                            emoji_get_imgfont_cb);
+    LV_ASSERT_NULL(imgfont);
+
+    if(imgfont) {
+        g_emoji_font = imgfont;
+    }
+    else {
+        LV_LOG_ERROR("emoji_font create faild");
+    }
+#endif
+
+    g_init_flag = true;
 }
 
 void lvx_font_deinit(void)
 {
     fts_list_clear();
-    if(font_base_path) {
-        lv_mem_free(font_base_path);
+
+    if(g_font_base_path) {
+        lv_mem_free(g_font_base_path);
+        g_font_base_path = NULL;
     }
 
-    init_flag = false;
+#if defined(CONFIG_FONT_USE_EMOJI)
+    if(g_emoji_font) {
+        lv_imgfont_destroy(g_emoji_font);
+        g_emoji_font = NULL;
+    }
+#endif
+
+    g_init_flag = false;
 }
 
 void lvx_font_base_path_set(const char * path)
 {
     uint32_t len = strlen(path) + 1;
-    if(font_base_path) {
-        font_base_path = lv_mem_realloc(font_base_path, len);
-        LV_ASSERT_MALLOC(font_base_path);
+    if(g_font_base_path) {
+        g_font_base_path = lv_mem_realloc(g_font_base_path, len);
+        LV_ASSERT_MALLOC(g_font_base_path);
     }
     else {
-        font_base_path = lv_mem_alloc(len);
-        LV_ASSERT_MALLOC(font_base_path);
+        g_font_base_path = lv_mem_alloc(len);
+        LV_ASSERT_MALLOC(g_font_base_path);
     }
 
-    memcpy(font_base_path, path, len);
+    memcpy(g_font_base_path, path, len);
 }
 
 bool lvx_font_create_core(lv_ft_info_t * newfont)
@@ -131,17 +162,24 @@ bool lvx_font_create_core(lv_ft_info_t * newfont)
     lvx_font_init();
 
     if(newfont == NULL || newfont->name == NULL || newfont->weight == 0) {
+        LV_LOG_ERROR("newfont param error");
         return false;
     }
 
-    lv_ft_info_t * tmp = save_ft_to_fts(newfont);
-    if(tmp) {
-        newfont->font = tmp->font;
-        return true;
+    lv_ft_info_t * ft_info = save_ft_to_fts(newfont);
+    if(!ft_info) {
+        LV_LOG_ERROR("save_ft_to_fts() failed");
+        newfont->font = NULL;
+        return false;
     }
 
-    newfont->font = NULL;
-    return false;
+    newfont->font = ft_info->font;
+
+#if defined(CONFIG_FONT_USE_EMOJI)
+    newfont->font->fallback = g_emoji_font;
+#endif
+
+    return true;
 }
 
 lv_font_t * lvx_font_create(const char * name, uint16_t size, LV_FT_FONT_STYLE style)
@@ -151,13 +189,19 @@ lv_font_t * lvx_font_create(const char * name, uint16_t size, LV_FT_FONT_STYLE s
     newfont.name = name;
     newfont.style = style;
     newfont.weight = size;
-    lvx_font_create_core(&newfont);
+
+    if(!lvx_font_create_core(&newfont)) {
+        LV_LOG_ERROR("can't create font: %s", name);
+        return NULL;
+    }
+
     return newfont.font;
 }
 
 void lvx_font_destroy(lv_font_t * delfont)
 {
-    if(delfont == NULL) {
+    if(!delfont) {
+        LV_LOG_WARN("delfont is NULL");
         return;
     }
 
@@ -176,7 +220,7 @@ static char * save_name_to_names(const char * name)
         return pos;
     }
 
-    name_refer_t * refer = _lv_ll_ins_tail(&font_tables.name_ll);
+    name_refer_t * refer = _lv_ll_ins_tail(&g_font_tables.name_ll);
     if(refer) {
         uint32_t len = strlen(name) + 1;
         refer->name = lv_mem_alloc(len);
@@ -185,7 +229,7 @@ static char * save_name_to_names(const char * name)
             refer->cnt = 1;
             return refer->name;
         }
-        _lv_ll_remove(&font_tables.name_ll, refer);
+        _lv_ll_remove(&g_font_tables.name_ll, refer);
         lv_mem_free(refer);
     }
     LV_LOG_WARN("save_name_to_names error(not memory).");
@@ -199,14 +243,13 @@ static char * save_name_to_names(const char * name)
  */
 static char * name_in_names_use(const char * name)
 {
-
-    name_refer_t * refer = _lv_ll_get_head(&font_tables.name_ll);
+    name_refer_t * refer = _lv_ll_get_head(&g_font_tables.name_ll);
     while(refer) {
         if(strcmp(refer->name, name) == 0) {
             refer->cnt += 1;
             return refer->name;
         }
-        refer = _lv_ll_get_next(&font_tables.name_ll, refer);
+        refer = _lv_ll_get_next(&g_font_tables.name_ll, refer);
     }
     return NULL;
 }
@@ -216,21 +259,21 @@ static char * name_in_names_use(const char * name)
  */
 static void name_in_names_del(const char * name)
 {
-    name_refer_t * refer = _lv_ll_get_head(&font_tables.name_ll);
+    name_refer_t * refer = _lv_ll_get_head(&g_font_tables.name_ll);
     while(refer) {
         if(strcmp(refer->name, name) == 0) {
             refer->cnt -= 1;
             if(refer->cnt <= 0) {
-                _lv_ll_remove(&font_tables.name_ll, refer);
+                _lv_ll_remove(&g_font_tables.name_ll, refer);
                 lv_mem_free(refer->name);
                 lv_mem_free(refer);
             }
             return;
         }
-        refer = _lv_ll_get_next(&font_tables.name_ll, refer);
+        refer = _lv_ll_get_next(&g_font_tables.name_ll, refer);
     }
 
-    LV_LOG_WARN("name_in_names_del error(not find:%p).", name);
+    LV_LOG_WARN("name_in_names_del error, can't find:%s", name);
 }
 
 /**
@@ -247,6 +290,7 @@ static lv_ft_info_t * save_ft_to_fts(const lv_ft_info_t * newfont)
 
     const uint16_t buff_len = 256;
     char * buff = lv_mem_alloc(buff_len);
+    LV_ASSERT_NULL(buff);
     if(!buff) {
         return NULL;
     }
@@ -261,7 +305,7 @@ static lv_ft_info_t * save_ft_to_fts(const lv_ft_info_t * newfont)
         tmp_font.style = newfont->style;
         tmp_font.weight = newfont->weight;
         if(lv_ft_font_init(&tmp_font)) {
-            refer = _lv_ll_ins_tail(&font_tables.ft_ll);
+            refer = _lv_ll_ins_tail(&g_font_tables.ft_ll);
             if(refer) {
                 refer->ft.name = save_name_to_names(newfont->name);
                 refer->ft.font = tmp_font.font;
@@ -287,7 +331,7 @@ static lv_ft_info_t * save_ft_to_fts(const lv_ft_info_t * newfont)
  */
 static lv_ft_info_t * ft_in_fts_use(const lv_ft_info_t * curfont)
 {
-    font_refer_t * refer = _lv_ll_get_head(&font_tables.ft_ll);
+    font_refer_t * refer = _lv_ll_get_head(&g_font_tables.ft_ll);
     while(refer) {
         if(refer->ft.weight == curfont->weight &&
            refer->ft.style == curfont->style &&
@@ -295,7 +339,7 @@ static lv_ft_info_t * ft_in_fts_use(const lv_ft_info_t * curfont)
             refer->cnt += 1;
             return &refer->ft;
         }
-        refer = _lv_ll_get_next(&font_tables.ft_ll, refer);
+        refer = _lv_ll_get_next(&g_font_tables.ft_ll, refer);
     }
     return NULL;
 }
@@ -306,19 +350,24 @@ static lv_ft_info_t * ft_in_fts_use(const lv_ft_info_t * curfont)
  */
 static void ft_in_fts_del(const lv_font_t * delfont)
 {
-    font_refer_t * refer = _lv_ll_get_head(&font_tables.ft_ll);
+    if(!delfont) {
+        LV_LOG_ERROR("delfont is NULL");
+        return;
+    }
+
+    font_refer_t * refer = _lv_ll_get_head(&g_font_tables.ft_ll);
     while(refer) {
         if(refer->ft.font == delfont) {
             refer->cnt -= 1;
             if(refer->cnt <= 0) {
                 lv_ft_font_destroy(refer->ft.font);
                 name_in_names_del(refer->ft.name);
-                _lv_ll_remove(&font_tables.ft_ll, refer);
+                _lv_ll_remove(&g_font_tables.ft_ll, refer);
                 lv_mem_free(refer);
             }
             return;
         }
-        refer = _lv_ll_get_next(&font_tables.ft_ll, refer);
+        refer = _lv_ll_get_next(&g_font_tables.ft_ll, refer);
     }
 
     LV_LOG_WARN("ft_in_fts_del error(not find:%p).", delfont);
@@ -329,13 +378,13 @@ static void ft_in_fts_del(const lv_font_t * delfont)
  */
 static void fts_list_clear(void)
 {
-    font_refer_t * refer = _lv_ll_get_head(&font_tables.ft_ll);
+    font_refer_t * refer = _lv_ll_get_head(&g_font_tables.ft_ll);
     while(refer) {
         lv_ft_font_destroy(refer->ft.font);
         name_in_names_del(refer->ft.name);
-        refer = _lv_ll_get_next(&font_tables.ft_ll, refer);
+        refer = _lv_ll_get_next(&g_font_tables.ft_ll, refer);
     }
-    _lv_ll_clear(&font_tables.ft_ll);
+    _lv_ll_clear(&g_font_tables.ft_ll);
 }
 
 /**
@@ -347,7 +396,7 @@ static void fts_list_clear(void)
  */
 static bool font_name_combine(char * buff, uint16_t buff_len, const char * name)
 {
-    snprintf(buff, buff_len, "%s/%s.%s", font_base_path, name, "ttf");
+    snprintf(buff, buff_len, "%s/%s.%s", g_font_base_path, name, "ttf");
     if(access(buff, F_OK) != 0) {
         LV_LOG_WARN("Font file not found.please check %s", buff);
         return false;
@@ -355,4 +404,27 @@ static bool font_name_combine(char * buff, uint16_t buff_len, const char * name)
     return true;
 }
 
-#endif
+#if defined(CONFIG_FONT_USE_EMOJI)
+
+#define IS_EMOJI(letter) \
+    (letter >=  CONFIG_FONT_EMOJI_UNICODE_START \
+  && letter <= (CONFIG_FONT_EMOJI_UNICODE_START + CONFIG_FONT_EMOJI_UNICODE_TOTAL))
+
+static bool emoji_get_imgfont_cb(const lv_font_t* font, void* img_src, uint16_t len,
+                                 uint32_t unicode, uint32_t unicode_next)
+{
+    LV_UNUSED(unicode_next);
+
+    if (IS_EMOJI(unicode)) {
+        char* path = (char*)img_src;
+        lv_snprintf(path, len,
+                    CONFIG_FONT_EMOJI_PATH "/%" PRIu32 "." CONFIG_FONT_EMOJI_EXT_NAME, unicode);
+        return true;
+    }
+
+    return false;
+}
+
+#endif /* CONFIG_FONT_USE_EMOJI */
+
+#endif /* LVX_USE_FONT_MANAGER */
