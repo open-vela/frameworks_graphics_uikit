743041f0ea1717c237cdcd88f6a37509c0479a12
pengyiqiang <pengyiqiang@xiaomi.com>
2022-07-20 19:55:25 +0800

    lvgl/ext: add lvx_async
    Change-Id: Ib1e7f36ea72dcad711562f119d6c21961774e4a9

---
+++ lvx_async.h
@@ -0,0 +1,52 @@
+/**
+ * @file lvx_async.h
+ *
+ */
+
+#ifndef LVX_ASYNC_H
+#define LVX_ASYNC_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*********************
+ *      INCLUDES
+ *********************/
+#include <lvgl/lvgl.h>
+
+/*********************
+ *      DEFINES
+ *********************/
+
+/**********************
+ *      TYPEDEFS
+ **********************/
+
+/**********************
+ * GLOBAL PROTOTYPES
+ **********************/
+
+/**
+ * Call an asynchronous function after next refresh.
+ * @param async_xcb a callback which is the task itself.
+ * @param user_data custom parameter
+ */
+lv_res_t lvx_async_after_refr_call(lv_async_cb_t async_xcb, void * user_data);
+
+/**
+ * Cancel calling the async function after the next refresh.
+ * @param async_xcb a callback which is the task itself.
+ * @param user_data custom parameter
+ */
+lv_res_t lvx_async_after_refr_call_cancel(lv_async_cb_t async_xcb, void * user_data);
+
+/**********************
+ *      MACROS
+ **********************/
+
+#ifdef __cplusplus
+} /*extern "C"*/
+#endif
+
+#endif /*LVX_ASYNC_H*/
