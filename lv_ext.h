5ca9ed0a34d37ed00e63a6e1075a8b2e3ab2842f
HaoHangyu <haohangyu@xiaomi.com>
2022-10-26 12:01:39 +0800

    apps/lvgl:replace lvx_widget to miwear/apps.
    neededby:874978
    
    Signed-off-by: HaoHangyu <haohangyu@xiaomi.com>

--- lv_ext.h
+++ lv_ext.h
@@ -2,15 +2,6 @@
 #define LV_EXT_H
 
 #include "lv_ext_conf.h"
-#include "lvx_widgets/lvx_common.h"
-#include "lvx_themes/lvx_theme_extended.h"
-#include "lvx_widgets/lvx_btn.h"
-#include "lvx_widgets/lvx_msgbox.h"
-#include "lvx_widgets/lvx_radio.h"
-#include "lvx_widgets/lvx_picker.h"
-#include "lvx_widgets/lvx_switch.h"
-#include "lvx_widgets/lvx_arc_scrollbar.h"
-#include "lvx_widgets/lvx_chart.h"
 #include "lvx_font_manager.h"
 #include "lvx_async.h"
 #include "rle/lvx_rle_decoder.h"
