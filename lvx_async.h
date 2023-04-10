41798762c07ba1e1aecbf13ee0e100d56f61a50d
pengyiqiang <pengyiqiang@xiaomi.com>
2023-04-10 20:16:12 +0800

    ext/lvx_async: add lvx_async_before_refr_call
    Signed-off-by: pengyiqiang <pengyiqiang@xiaomi.com>

--- lvx_async.h
+++ lvx_async.h
@@ -28,6 +28,20 @@ extern "C" {
  **********************/
 
 /**
+ * Call an asynchronous function before next refresh.
+ * @param async_xcb a callback which is the task itself.
+ * @param user_data custom parameter
+ */
+lv_res_t lvx_async_before_refr_call(lv_async_cb_t async_xcb, void * user_data);
+
+/**
+ * Cancel calling the async function before the next refresh.
+ * @param async_xcb a callback which is the task itself.
+ * @param user_data custom parameter
+ */
+lv_res_t lvx_async_before_refr_call_cancel(lv_async_cb_t async_xcb, void * user_data);
+
+/**
  * Call an asynchronous function after next refresh.
  * @param async_xcb a callback which is the task itself.
  * @param user_data custom parameter
