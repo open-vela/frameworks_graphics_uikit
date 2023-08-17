953252b0ed69cf424ae8efc9d1c37cc1e8bade7b
pengyiqiang <pengyiqiang@xiaomi.com>
2023-08-17 15:42:35 +0800

    feat(demo): add smart band demo
    Signed-off-by: pengyiqiang <pengyiqiang@xiaomi.com>
    Signed-off-by: jianglianfang <jianglianfang@xiaomi.com>

---
+++ test/bandx/page/page.h
@@ -0,0 +1,66 @@
+/**
+ * @file page.h
+ *
+ */
+
+#ifndef PAGE_H
+#define PAGE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*********************
+ *      INCLUDES
+ *********************/
+#include <lvgl/lvgl.h>
+
+#ifdef CONFIG_LVX_USE_DEMO_BANDX
+
+#include "../resource/resource.h"
+
+/*********************
+ *      DEFINES
+ *********************/
+
+#define PAGE_CLASS_DEF(NAME)                         \
+    const lv_fragment_class_t _page_##NAME##_cls = { \
+                                                     .constructor_cb = on_page_construct,         \
+                                                     .destructor_cb = on_page_destruct,           \
+                                                     .attached_cb = on_page_attached,             \
+                                                     .detached_cb = on_page_detached,             \
+                                                     .create_obj_cb = on_page_create,             \
+                                                     .obj_created_cb = on_page_created,           \
+                                                     .obj_will_delete_cb = on_page_will_delete,   \
+                                                     .obj_deleted_cb = on_page_deleted,           \
+                                                     .event_cb = on_page_event,                   \
+                                                     .instance_size = sizeof(page_ctx_t)          \
+                                                   }
+
+/**********************
+ *      TYPEDEFS
+ **********************/
+
+/**********************
+ * GLOBAL PROTOTYPES
+ **********************/
+
+void page_init(void);
+
+lv_fragment_t * page_create(const char * name, void * arg);
+
+bool page_push(lv_fragment_t * self, const char * name, void * arg);
+
+void page_pop(lv_fragment_t * self);
+
+/**********************
+ *      MACROS
+ **********************/
+
+#endif /*CONFIG_LVX_USE_DEMO_BANDX*/
+
+#ifdef __cplusplus
+} /*extern "C"*/
+#endif
+
+#endif /*PAGE_H*/
