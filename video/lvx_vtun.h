39dd6fb050f5c77f52c8f44acb300959a5d8808e
jianglianfang <jianglianfang@xiaomi.com>
2023-09-14 15:48:07 +0800

    ext/video:add crop for video widgets
    Signed-off-by: jianglianfang <jianglianfang@xiaomi.com>

--- video/lvx_vtun.h
+++ video/lvx_vtun.h
@@ -20,7 +20,15 @@ typedef enum {
 } lvx_vtun_ctrl_evt_type;
 
 typedef struct {
+    size_t x1;
+    size_t x2;
+    size_t y1;
+    size_t y2;
+} lvx_vtun_crop_info;
+
+typedef struct {
     lvx_vtun_frame_format format;
+    lvx_vtun_crop_info crop_info;
     unsigned current_ms;
     size_t size;
     int stride;
