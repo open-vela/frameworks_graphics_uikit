61490b92893b7fcbe44a21714a05e515bb5e1d2c
pengyiqiang <pengyiqiang@xiaomi.com>
2023-11-01 12:03:39 +0800

    ext/font_manager: remove redundant FONT_LOG conv
    Open LV_LOG_INFO to print all LOGs of font manager and remove redundant designs.
    
    Signed-off-by: pengyiqiang <pengyiqiang@xiaomi.com>

--- font_manager/font_config.h
+++ font_manager/font_config.h
@@ -27,75 +27,67 @@ extern "C" {
 /* FONT_PATH */
 
 #if defined(CONFIG_FONT_LIB_PATH)
-#  define FONT_LIB_PATH CONFIG_FONT_LIB_PATH
+#define FONT_LIB_PATH CONFIG_FONT_LIB_PATH
 #else
-#  define FONT_LIB_PATH "/"
+#define FONT_LIB_PATH "/"
 #endif
 
 /* FONT_NAME */
 
 #if defined(CONFIG_FONT_NAME_MAX)
-#  define FONT_NAME_MAX CONFIG_FONT_NAME_MAX
+#define FONT_NAME_MAX CONFIG_FONT_NAME_MAX
 #else
-#  define FONT_NAME_MAX 32
+#define FONT_NAME_MAX 32
 #endif
 
 /* FONT_EXT */
 
 #if defined(CONFIG_FONT_EXT_NAME)
-#  define FONT_EXT_NAME CONFIG_FONT_EXT_NAME
+#define FONT_EXT_NAME CONFIG_FONT_EXT_NAME
 #else
-#  define FONT_EXT_NAME "ttf"
+#define FONT_EXT_NAME "ttf"
 #endif
 
 #if defined(CONFIG_FONT_EXT_MAX)
-#  define FONT_EXT_MAX CONFIG_FONT_EXT_MAX
+#define FONT_EXT_MAX CONFIG_FONT_EXT_MAX
 #else
-#  define FONT_EXT_MAX 8
+#define FONT_EXT_MAX 8
 #endif
 
 /* FONT_CACHE */
 
 #if defined(CONFIG_FONT_CACHE_SIZE)
-#  define FONT_CACHE_SIZE CONFIG_FONT_CACHE_SIZE
+#define FONT_CACHE_SIZE CONFIG_FONT_CACHE_SIZE
 #else
-#  define FONT_CACHE_SIZE 8
+#define FONT_CACHE_SIZE 8
 #endif
 
 /* FONT_FAMILY */
 
 #if defined(CONFIG_FONT_USE_FONT_FAMILY)
-#  define FONT_USE_FONT_FAMILY CONFIG_FONT_USE_FONT_FAMILY
+#define FONT_USE_FONT_FAMILY CONFIG_FONT_USE_FONT_FAMILY
 #else
-#  define FONT_USE_FONT_FAMILY 0
+#define FONT_USE_FONT_FAMILY 0
 #endif
 
 #if defined(CONFIG_FONT_CONFIG_FILE_PATH)
-#  define FONT_CONFIG_FILE_PATH CONFIG_FONT_CONFIG_FILE_PATH
+#define FONT_CONFIG_FILE_PATH CONFIG_FONT_CONFIG_FILE_PATH
 #else
-#  define FONT_CONFIG_FILE_PATH "/etc/font_config.json"
+#define FONT_CONFIG_FILE_PATH "/etc/font_config.json"
 #endif
 
 /* FONT_EMOJI */
 
 #if defined(CONFIG_FONT_USE_EMOJI)
-#  define FONT_USE_EMOJI CONFIG_FONT_USE_EMOJI
+#define FONT_USE_EMOJI CONFIG_FONT_USE_EMOJI
 #else
-#  define FONT_USE_EMOJI 0
+#define FONT_USE_EMOJI 0
 #endif
 
 #if defined(CONFIG_FONT_EMOJI_HEADER)
-#  define FONT_EMOJI_HEADER CONFIG_FONT_EMOJI_HEADER
+#define FONT_EMOJI_HEADER CONFIG_FONT_EMOJI_HEADER
 #else
-#  define FONT_EMOJI_HEADER "Emoji"
-#endif
-
-/* FONT_LOG */
-
-#if defined(CONFIG_FONT_USE_LOG_USER)
-#  define FONT_USE_LOG_USER CONFIG_FONT_USE_LOG_USER
-#else
-#  define FONT_USE_LOG_USER 0
+#define FONT_EMOJI_HEADER "Emoji"
 #endif
 
 /**********************
