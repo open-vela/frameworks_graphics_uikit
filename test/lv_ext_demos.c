ad5415e4dd5bf8520c103e040f4450812f32ab6f
jianglianfang <jianglianfang@xiaomi.com>
2023-11-23 11:32:57 +0800

    ext/test: add lv_ext_demo
    Change-Id: I0f9aa2a1fdfed73b23fe6e7a722bf1b2e33d04d4

---
+++ test/lv_ext_demos.c
@@ -0,0 +1,181 @@
+
+/**
+ * @file lv_ext_demo.c
+ *
+ */
+
+/*********************
+ *      INCLUDES
+ *********************/
+
+#include <nuttx/config.h>
+#include <unistd.h>
+
+#ifdef CONFIG_LV_USE_NUTTX_LIBUV
+#include <uv.h>
+#endif
+#include "../lv_ext.h"
+#include "lv_ext_demos.h"
+
+/*********************
+ *      DEFINES
+ *********************/
+
+#define LV_EXT_DEMOS_COUNT (sizeof(demos_entry_info) / sizeof(demo_entry_info_t) - 1)
+
+/**********************
+ *      TYPEDEFS
+ **********************/
+
+typedef void (*demo_method_cb)(char* info[], int size);
+
+typedef struct {
+    const char* name;
+    demo_method_cb entry_cb;
+} demo_entry_info_t;
+
+/**********************
+ *  STATIC PROTOTYPES
+ **********************/
+
+static bool lv_ext_demos(char* info[], int size);
+#ifdef CONFIG_LV_USE_NUTTX_LIBUV
+static void lv_nuttx_uv_loop(uv_loop_t* loop, lv_disp_t* disp, lv_indev_t* indev);
+#endif
+
+/**********************
+ *  STATIC VARIABLES
+ **********************/
+
+static const demo_entry_info_t demos_entry_info[] = {
+
+#ifdef CONFIG_LVX_USE_VIDEO_EXAMPLE
+    { "video", .entry_cb = lvx_example_video },
+    { "camera", .entry_cb = lvx_example_camera },
+    { "video_call", .entry_cb = lvx_example_video_call },
+    { "video_ctl", .entry_cb = lvx_example_video_controller },
+#endif
+
+    { "", .entry_cb = NULL }
+};
+
+/**********************
+ *      MACROS
+ **********************/
+
+/**********************
+ *   GLOBAL FUNCTIONS
+ **********************/
+
+int main(int argc, FAR char* argv[])
+{
+    lv_nuttx_dsc_t info;
+    lv_nuttx_result_t result;
+
+#ifdef CONFIG_LV_USE_NUTTX_LIBUV
+    uv_loop_t ui_loop;
+#endif
+
+    lv_init();
+
+    lv_nuttx_dsc_init(&info);
+    info.fb_path = CONFIG_LV_FBDEV_INTERFACE_DEFAULT_DEVICEPATH;
+    info.input_path = CONFIG_LV_TOUCHPAD_INTERFACE_DEFAULT_DEVICEPATH;
+
+    lv_nuttx_init(&info, &result);
+
+    lv_ext_init();
+
+    if (result.disp == NULL) {
+        LV_LOG_ERROR("lv_demos initialization failure!");
+        return 1;
+    }
+
+    if (!lv_ext_demos(&argv[1], argc - 1)) {
+
+        /* we can add custom demos here */
+
+        goto demo_end;
+    }
+
+#ifdef CONFIG_LV_USE_NUTTX_LIBUV
+    lv_nuttx_uv_loop(&ui_loop, result.disp, result.indev);
+#else
+    while (1) {
+        lv_timer_handler();
+        usleep(10 * 1000);
+    }
+#endif
+
+demo_end:
+    lv_disp_remove(result.disp);
+    lv_ext_deinit();
+    lv_deinit();
+
+    return 0;
+}
+
+/**********************
+ *   STATIC FUNCTIONS
+ **********************/
+
+#ifdef CONFIG_LV_USE_NUTTX_LIBUV
+static void lv_nuttx_uv_loop(uv_loop_t* loop, lv_disp_t* disp,
+    lv_indev_t* indev)
+{
+    lv_nuttx_uv_t uv_info;
+    void* data;
+
+    uv_loop_init(loop);
+
+    lv_memset(&uv_info, 0, sizeof(uv_info));
+    uv_info.loop = loop;
+    uv_info.disp = disp;
+    uv_info.indev = indev;
+
+    data = lv_nuttx_uv_init(&uv_info);
+    uv_run(loop, UV_RUN_DEFAULT);
+    lv_nuttx_uv_deinit(&data);
+}
+#endif
+
+static bool lv_ext_demos(char* info[], int size)
+{
+    const int demos_count = LV_EXT_DEMOS_COUNT;
+
+    if (demos_count <= 0) {
+        LV_LOG_ERROR("Please enable some lv_ext_demos firstly!");
+        return false;
+    }
+
+    const demo_entry_info_t* entry_info = NULL;
+
+    if (size > 0 && info) {
+        const char* name = info[0];
+        for (int i = 0; i < demos_count; i++) {
+            if (strcmp(name, demos_entry_info[i].name) == 0) {
+                entry_info = &demos_entry_info[i];
+            }
+        }
+    }
+
+    if (entry_info == NULL) {
+        LV_LOG_ERROR("lv_ext_demos create(%s) failure!", size > 0 ? info[0] : "");
+        goto err;
+    }
+
+    if (entry_info->entry_cb) {
+        entry_info->entry_cb(&info[1], size - 1);
+        return true;
+    }
+
+err:
+    LV_LOG("\nUsage: lv_ext_demos demo [parameters]\n");
+    LV_LOG("\ndemo list:\n");
+
+    for (int i = 0; i < demos_count; i++) {
+        LV_LOG("     %s \n", demos_entry_info[i].name);
+    }
+
+    return false;
+}
--- test/video/lvx_video_example.c
+++ test/video/lvx_video_example.c
@@ -1,35 +1,60 @@
+/**
+ * @file lvx_video_example.c
+ *
+ */
+
+/*********************
+ *      INCLUDES
+ *********************/
+
 #include "lvx_video_example.h"
 #include "lvx_camera_controller.h"
 #include "lvx_video_controller.h"
 #include <ext/video/lvx_video.h>
 #include <stdio.h>
 
+/*********************
+ *      DEFINES
+ *********************/
+
 #define DURATION 12 * 1000
+
+/**********************
+ *      TYPEDEFS
+ **********************/
+
+/**********************
+ *  STATIC PROTOTYPES
+ **********************/
+static void video_obj_free_cb(lv_timer_t* t);
+
+static void* video_parse_cmd(char* info[], int size);
+
+/**********************
+ *  STATIC VARIABLES
+ **********************/
+
+/**********************
+ *      MACROS
+ **********************/
+
+/**********************
+ *   GLOBAL FUNCTIONS
+ **********************/
 
 /****************************************************************************
  * lvx_example_video
  ****************************************************************************/
 
-static void video_obj_free_cb(lv_timer_t* t)
+void lvx_example_video(char* info[], int size)
 {
-    lv_obj_t* obj = t->user_data;
-    lv_obj_del(obj);
-    //播放第二遍
-    // lv_obj_t* video1 = lvx_video_create(lv_scr_act());
-    // lvx_video_set_src(video1, CONFIG_LVX_DEFAULT_VIDEO_PATH);
-    // lv_obj_align(video1, LV_ALIGN_TOP_LEFT, 0, 0);
-    // lvx_video_start(video1);
-}
+    char* src = video_parse_cmd(info, size);
 
-void lvx_example_video(void)
-{
     lv_obj_t* video = lvx_video_create(lv_scr_act());
     lv_timer_t* timer = lv_timer_create(video_obj_free_cb, DURATION, video);
     lv_timer_set_repeat_count(timer, 1);
     lv_timer_pause(timer);
-
-    lvx_video_set_src(video, CONFIG_LVX_DEFAULT_VIDEO_PATH);
-
+    lvx_video_set_src(video, src);
     lv_timer_resume(timer);
 
     lv_obj_align(video, LV_ALIGN_CENTER, 0, 0);
@@ -41,13 +66,14 @@ void lvx_example_video(void)
  * lvx_example_video_controller
  ****************************************************************************/
 
-void lvx_example_video_controller(void)
+void lvx_example_video_controller(char* info[], int size)
 {
+    char* src = video_parse_cmd(info, size);
+
     lv_obj_t* obj = lvx_video_controller_create(lv_scr_act());
 
     lv_obj_set_size(obj, LV_PCT(100), LV_PCT(100));
-
-    lvx_video_controller_set_src(obj, CONFIG_LVX_DEFAULT_VIDEO_PATH);
+    lvx_video_controller_set_src(obj, src);
 
     LV_IMG_DECLARE(img_play);
     LV_IMG_DECLARE(img_pause);
@@ -60,20 +86,14 @@ void lvx_example_video_controller(void)
  * lvx_example_video_call
  ****************************************************************************/
 
-static void video_call_cb(lv_timer_t* t)
+void lvx_example_video_call(char* info[], int size)
 {
-    lv_obj_t* obj = t->user_data;
-    lvx_video_stop(obj);
-    lv_obj_del(obj);
-}
-
-void lvx_example_video_call(void)
-{
+    char* src = video_parse_cmd(info, size);
     lv_obj_t* camera_obj = lvx_video_create(lv_scr_act());
     lvx_video_set_src(camera_obj, "Camera:");
     lv_obj_set_size(camera_obj, LV_PCT(100), LV_PCT(100));
 
-    lv_timer_t* timer1 = lv_timer_create(video_call_cb, DURATION, camera_obj);
+    lv_timer_t* timer1 = lv_timer_create(video_obj_free_cb, DURATION, camera_obj);
     lv_timer_set_repeat_count(timer1, 1);
     lv_timer_pause(timer1);
 
@@ -82,9 +102,9 @@ void lvx_example_video_call(void)
     lv_obj_set_style_border_width(video_obj, 2, 0);
     lv_obj_set_style_border_color(video_obj, lv_color_hex(0xffffff), 0);
 
-    lvx_video_set_src(video_obj, CONFIG_LVX_DEFAULT_VIDEO_PATH);
+    lvx_video_set_src(video_obj, src);
 
-    lv_timer_t* timer = lv_timer_create(video_call_cb, DURATION, video_obj);
+    lv_timer_t* timer = lv_timer_create(video_obj_free_cb, DURATION, video_obj);
     lv_timer_set_repeat_count(timer, 1);
     lv_timer_pause(timer);
 
@@ -100,9 +120,32 @@ void lvx_example_video_call(void)
 /****************************************************************************
  * lvx_example_camera
  ****************************************************************************/
-void lvx_example_camera(void)
+void lvx_example_camera(char* info[], int size)
 {
     lv_obj_t* obj = lvx_camera_controller_create(lv_scr_act());
+    if (size > 0 && info) {
+        lvx_camera_controller_set_url(obj, info[0]);
+    }
     lv_obj_set_size(obj, LV_PCT(100), LV_PCT(100));
     lv_obj_align(obj, LV_ALIGN_CENTER, 0, 0);
+}
+
+/**********************
+ *   STATIC FUNCTIONS
+ **********************/
+
+static void video_obj_free_cb(lv_timer_t* t)
+{
+    lv_obj_t* obj = t->user_data;
+    lvx_video_stop(obj);
+    lv_obj_del(obj);
+}
+
+static void* video_parse_cmd(char* info[], int size)
+{
+    if (size > 0 && info) {
+        return info[0];
+    } else {
+        return CONFIG_LVX_DEFAULT_VIDEO_PATH;
+    }
 }
