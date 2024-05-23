9b9bbc479127c166096fefa13e15a6b199ec3b31
zhangyan63 <zhangyan63@xiaomi.com>
2024-05-23 21:05:17 +0800

    lvgl ext adds basic object creation test cases
    Des:
    Verification steps:
    
    Signed-off-by: zhangyan63 <zhangyan63@xiaomi.com>

---
+++ test/creation/lv_create_obj.h
@@ -0,0 +1,40 @@
+/**
+ * @file lvx_create_obj.h
+ *
+ */
+
+#ifndef LV_CREATE_OBJ_H
+#define LV_CREATE_OBJ_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*********************
+ *      INCLUDES
+ *********************/
+
+#include <lvgl/lvgl.h>
+
+/*********************
+ *      DEFINES
+ *********************/
+
+/**********************
+ *      TYPEDEFS
+ **********************/
+
+/**********************
+ * GLOBAL PROTOTYPES
+ **********************/
+void lvx_time_obj_creation(char* info[], int size, void* param);
+
+/**********************
+ *      MACROS
+ **********************/
+
+#ifdef __cplusplus
+} /*extern "C"*/
+#endif
+
+#endif
