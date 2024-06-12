8f8fc9c899b3da9ea7255c295621ab71ab3d99c9
zhangchao53 <zhangchao53@xiaomi.com>
2024-06-12 11:53:39 +0800

    update Copyright year
    Change-Id: I8e4717fedbda6485107dad9df568308ef0af2187

--- animengine/lvx_animengine_adapter.c
+++ animengine/lvx_animengine_adapter.c
@@ -1,7 +1,7 @@
 /*
  * ext/animengine/lvx_animengine_adapter.h
  *
- * Copyright (C) 2023 Xiaomi Corporation
+ * Copyright (C) 2024 Xiaomi Corporation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
