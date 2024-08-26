d3f6aaf810c18ba4d0b28a6c33bc35311d3ddfcf
zhangjipeng <zhangjipeng@xiaomi.com>
2024-08-26 19:12:18 +0800

    markdown: add markdown widget
    Signed-off-by: xinbingnan <xinbingnan@xiaomi.com>

---
+++ test/markdown/markdown_test.h
@@ -0,0 +1,21 @@
+/**
+ * @file markdown_test.h
+ *
+ */
+#ifndef LVX_MARKDOWN_TEST_H
+#define LVX_MARKDOWN_TEST_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**********************
+ * GLOBAL PROTOTYPES
+ **********************/
+void lvx_markdown_demo(char * info[], int size, void * param);
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif /*LVX_MARKDOWN_TEST_H*/
